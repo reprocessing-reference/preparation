41ced729881e9502df6d729288b2e363c34b6392
Esquis Benjamin <benjamin.esquis@csgroup.eu>
2020-12-02 16:07:53 +0100

    Added compose script

---
+++ ReproBaseAPI/distribution/ReproBaseline-01.00.00/launch.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+
+# Launch script for the service
+
+CUR_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+
+java -jar $CUR_DIR/lib/rba-service-1.0-SNAPSHOT.jar --spring.config.location=file:$CUR_DIR/conf/ $*
