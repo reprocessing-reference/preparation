96cb2a5bf51ed746d1916535cf99baefbf5c7718
Yannick Peyrotau <yannick.peyrotau@csgroup.eu>
2020-12-03 13:35:55 +0000

    build(compose) : https configuration working

--- ReproBaseAPI/distribution/compose/init-letsencrypt.sh
+++ ReproBaseAPI/distribution/compose/init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=(reprocessing-preparation.ml)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="yannick.peyrotau@csgroup.eu" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -77,4 +77,4 @@ docker-compose run --rm --entrypoint "\
 echo
 
 echo "### Reloading nginx ..."
-docker-compose exec proxy proxy -s reload
+docker-compose exec proxy nginx -s reload
