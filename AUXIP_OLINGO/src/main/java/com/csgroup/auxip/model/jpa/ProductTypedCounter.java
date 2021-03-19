ac9bf55cac5c40824df2338bb3f797fb0d65e870
Esquis Benjamin <benjamin.esquis@csgroup.eu>
2021-03-19 09:46:09 +0100

    Modified ProductTypeCounter

--- AUXIP_OLINGO/src/main/java/com/csgroup/auxip/model/jpa/ProductTypedCounter.java
+++ AUXIP_OLINGO/src/main/java/com/csgroup/auxip/model/jpa/ProductTypedCounter.java
@@ -11,10 +11,32 @@ import javax.persistence.Embeddable;
 public class ProductTypedCounter {
     // product Type
      private String productType;
+    // product Type
+     private String plateForm;
+    // product Type
+     private String unit;
     //  can be one of the following : volume,completed,failed
      private CounterType counterType;
     //  value of the counter 
      private long value;
+     
+	public String getProductType() {
+		return productType;
+	}
+	public String getPlateForm() {
+		return plateForm;
+	}
+	public String getUnit() {
+		return unit;
+	}
+	public CounterType getCounterType() {
+		return counterType;
+	}
+	public long getValue() {
+		return value;
+	}
+     
+     
 
 
 }
