8dd830724502fab503eabfa24fe9b89d77b141ab
benjamin.esquis <benjamin.esquis@csgroup.eu>
2021-04-01 13:14:32 +0000

    Added script to build all dockers

---
+++ build_all.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+
+CUR_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+
+
+cd ${CUR_DIR}/AUXIP_OLINGO/distribution
+
+./build_docker.bash
+
+cd ${CUR_DIR}/ReproBaseAPI/distribution/build/
+
+./build_docker.bash
+
+cd ${CUR_DIR}/AUXIP/distribution
+
+./build_docker.bash
+
+cd ${CUR_DIR}/RollingArchive
+
+docker build -t rollingarchive:1.0.0 .
+
+cd ${CUR_DIR}
