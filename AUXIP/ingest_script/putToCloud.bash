c841f8bf0afd4510a8ba6d2b1b4b4a8b51278dff
Esquis Benjamin <benjamin.esquis@csgroup.eu>
2021-06-16 08:07:15 +0200

    Removed old AUXIP project

--- AUXIP/ingest_script/putToCloud.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-for f in $(find $1 -type f); do echo $f; uuid=$(uuidgen); echo " : "$uuid;mkdir /DATA2/auxip_s3/$uuid;cp $f /DATA2/auxip_s3/$uuid/; echo $(basename $f)" : "$uuid >> s2_uuid_listing.txt;done
