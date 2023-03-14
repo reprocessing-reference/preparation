46ae31f28fcb4650cb4cac0052a00872e30277dc
rponthon <rodolpheponthon@csgroup.eu>
2023-03-14 14:00:40 +0000

    Correction des anciens certificats

--- compose/init-letsencrypt.sh
+++ compose/init-letsencrypt.sh
@@ -8,7 +8,7 @@ fi
 domains=(reprocessing-auxiliary.copernicus.eu)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="yannick.peyrotau@csgroup.eu" # Adding a valid address is strongly recommended
+email="" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
